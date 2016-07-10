27792f2eb225cbde324a89554354638dbe8418e0
adnan akgun <adnan.akgun@netclipper.co.uk>
2016-07-10 18:15:27 +0100

    bug fixes

--- platforms/android/assets/www/app/app.js
+++ platforms/android/assets/www/app/app.js
@@ -1,16 +1,4 @@
 (function() {
 	"use strict";
 	var easyScramblerApp = angular.module('easyScrambler', ['ngclipboard']);
-	var app = {
-		// Application Constructor
-		initialize: function() {
-			this.bindEvents();
-		},
-		bindEvents: function() {
-			document.addEventListener('deviceready', this.onDeviceReady, false);
-		},
-		onDeviceReady: function() {
-			app.receivedEvent('deviceready');
-		}
-	};
 })();
